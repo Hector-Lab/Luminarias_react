4e4bb0164acdd4e620c603f85ed45fa7007e655b
Hector Ramirez <56307732+Hector-Lab@users.noreply.github.com>
2021-12-13 12:37:06 -0600

    Iconos del bottom tab

--- Styles/Color.ts
+++ Styles/Color.ts
@@ -1,2 +1,3 @@
 export const SuinpacRed = "#B20115";
 export const BackgrounBlue = "#ffe4e1";
+export const iconColorBlue = "#90a4ae";
--- pages/Luminarias/Luminarias-pages/home.tsx
+++ pages/Luminarias/Luminarias-pages/home.tsx
@@ -1,17 +1,40 @@
 import React,{} from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Luminarias from "./Luminarias";
 import LuminariasEstados from "./estado-luminarias";
 import CustomMap from './map';
+import { iconColorBlue, SuinpacRed } from "../../../Styles/Color";
 export default function MainLuminaria(props:any ){
     const Tabs = createBottomTabNavigator();
     return (
         <NavigationContainer independent = {true}>
-            <Tabs.Navigator>
+            <Tabs.Navigator
+                screenOptions = {({route})=>({
+                    tabBarIcon:({ focused, color, size }) =>{
+                        let iconName;
+
+                        if (route.name === 'Home') {
+                          iconName = focused
+                            ? "home"
+                            : 'home';
+                        } else if (route.name === 'Luminaria') {
+                          iconName = focused ? 'bulb' : 'bulb';
+                        }else if (route.name === 'Historia'){
+                            iconName = focused ? 'time' : 'time';
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    }
+                })}
+                tabBarOptions={{
+                    activeTintColor: SuinpacRed,
+                    inactiveTintColor: iconColorBlue,
+                  }}
+            >
                 <Tabs.Screen name="Home" component={CustomMap} />
                 <Tabs.Screen name="Luminaria" component={Luminarias}/>
-                <Tabs.Screen name="Estado" component={LuminariasEstados}/>
+                <Tabs.Screen name="Historia" component={LuminariasEstados}/>
             </Tabs.Navigator>
     </  NavigationContainer>
     )
