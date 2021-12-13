27a04b24729d7436606d88973d207a70e626b509
Hector-Lab <56307732+Hector-Lab@users.noreply.github.com>
2021-12-12 21:07:18 -0600

    Log in sin botton tab

---
+++ Styles/styles.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native';
+import {SuinpacRed} from "./Color";
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+    },
+    containerTest1:{
+        flex: 1,
+        backgroundColor:"red",
+    },
+    containerTest2:{
+        flex: 1,
+        backgroundColor:"green",
+    },
+    containerTest3:{
+        flex: 1,
+        backgroundColor:"blue",
+    },
+    avatarElement : {
+        flex: 1,
+        alignItems: 'center'
+      },
+    avatarView: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    },
+    inputButtons : {
+        flex: 3,
+        flexDirection: 'column',
+        margin:30,
+    },
+    btnButton: {
+        marginTop: 30,
+        borderRadius: 15,
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: 'white',
+        borderWidth : 2,
+        borderColor: SuinpacRed,
+        //backgroundColor: 'red'
+    },
+    btnTexto: {
+        color: SuinpacRed ,
+        fontWeight: 'bold'
+      },
+    FooterConteiner: {
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    FooterText: {
+        color: SuinpacRed,
+        fontSize: 23
+    }
+});
+
+export default Styles;
---
+++ pages/Login.tsx
@@ -0,0 +1,46 @@
+import { TouchableOpacity, View, StyleSheet } from "react-native";
+import React from "react";
+import { Text } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import Styles from "../Styles/styles";
+import { Avatar } from "react-native-elements/dist/avatar/Avatar";
+import { Input } from "react-native-elements/dist/input/Input";
+
+export default function Log(props: any) {
+    const login = ()=>{
+        props.navigation.navigate("Luminarias");
+    }
+
+    return (
+        <View style={Styles.container}>
+            <StatusBar style="auto" />
+            <View style={Styles.avatarView}>
+                <View style={Styles.avatarElement}>
+                    <Avatar 
+                        rounded
+                        size = "xlarge"
+                        containerStyle = {{height:100,width:200}}
+                        source = {require("../resources/suinpac.png")} //FIXME: se puede cambiar por el logo de mexico
+                    />
+                </View>
+            </View>
+            <View style = {Styles.inputButtons} >
+                <Input 
+                    placeholder = "Nombre de usuario"
+                    onChangeText = {text => ("")}
+                    leftIcon = {{type:'font-awesome', name: 'user'}}
+                />
+                <Input 
+                    placeholder = "Contraseña"
+                    onChangeText = {pass =>("")}
+                    leftIcon = {{type:'font-awesome', name: 'lock'}}/>
+                <TouchableOpacity style={Styles.btnButton} onPress={login}>
+                    <Text style = {Styles.btnTexto} >Acceder</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {Styles.FooterConteiner}>
+                <Text style = {Styles.FooterText} >SUINPAC</Text>
+            </View>
+        </View>
+      );
+}
