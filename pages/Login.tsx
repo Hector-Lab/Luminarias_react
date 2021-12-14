7aa8e0c0c8665a14ad5f4e1d11c06af0f2c47da4
Hector Ramirez <56307732+Hector-Lab@users.noreply.github.com>
2021-12-14 17:26:43 -0600

    Log

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -6,14 +6,44 @@ import Styles from "../Styles/styles";
 import { Avatar } from "react-native-elements/dist/avatar/Avatar";
 import { Auth } from './controller/api-controller';
 import Loading from './components/modal-loading';
+import Message from './components/modal-message';
+import { SuinpacRed } from "../Styles/Color";
+
 export default function Log(props: any) {
     const [user,setUser] = useState(String);
     const [password,setPassword] = useState(String);
     const [loading,setLoading] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+    const [message,setMessage] = useState(String);
+    const [tittle,setTittle] = useState(String);
     const login = async ()=>{
-        setLoading(true);
-        await Auth(user,password);
-        //props.navigation.navigate("Menu");
+       /* let validDatos = true;
+        if( user == "" ){
+            setTittle("Mensaje");
+            setMessage("Favor de ingresar su nombre de usuario");
+            validDatos = false;
+        }
+        if (password == ""){
+            setTittle("Mensaje");
+            setMessage("Favor de ingresar su contraseña");
+            validDatos = false;
+        }
+        setShowMessage(!validDatos);
+        console.log("Valid datos" + validDatos);
+        if(validDatos){
+            await Auth(user,password)
+            .then((result)=>{
+                console.log(result);
+            })
+            .catch(()=>{
+                console.log("Error");
+                //NOTE: manejo de errores
+            })
+        }*/
+        //setLoading(true);
+        
+        props.navigation.navigate("Menu");
+
     }
 
     return (    
@@ -38,13 +68,14 @@ export default function Log(props: any) {
                 <Input 
                     autoCompleteType = {"username"}
                     placeholder = "Nombre de usuario"
-                    onChangeText = {text => ("")}
+                    onChangeText = {text => (setUser(text))}
                     leftIcon = {{type:'font-awesome', name: 'user'}}
                 />
                 <Input 
+                    secureTextEntry = {true} 
                     autoCompleteType = {"password"}
                     placeholder = "Contraseña"
-                    onChangeText = {pass =>("")}
+                    onChangeText = {pass =>(setPassword(pass))}
                     leftIcon = {{type:'font-awesome', name: 'lock'}}/>
                 <TouchableOpacity style={Styles.btnButton} onPress={login}>
                     <Text style = {Styles.btnTexto} >Acceder</Text>
@@ -53,10 +84,22 @@ export default function Log(props: any) {
         </View>
             </ScrollView>
             <Loading 
-          transparent = {true}
-          loading = {loading}
-          loadinColor = {"#0000ff"}
-          onCancelLoad = {()=>{}}
+                transparent = {true}
+                loading = {loading}
+                loadinColor = {"#0000ff"}
+                onCancelLoad = {()=>{}}
+            />
+         <Message
+            transparent = {true}
+            loading = {showMessage}
+            onCancelLoad = {()=>{ setShowMessage(false) }}
+            color = {SuinpacRed}
+            icon = {"alert-triangle"}
+            iconsource = "feather"
+            loadinColor = {SuinpacRed}
+            message = {"Campos Vacios!!"}
+            tittle = {"Mensaje"}
+            buttonText = {"Aceptar"}
          />
         </KeyboardAvoidingView>
       );
