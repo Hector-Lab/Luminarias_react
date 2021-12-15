51b9300c5d31940395ea2d5a41d1624348e7eacb
Hector Ramirez <56307732+Hector-Lab@users.noreply.github.com>
2021-12-15 13:07:55 -0600

    storage arregladoÃ

--- pages/controller/storage-controller.ts
+++ pages/controller/storage-controller.ts
@@ -6,6 +6,53 @@ export class StorageService{
     createOpenDB () {
         db = SQLite.openDatabase("data.db");
     }
+     createTables (){
+        db.transaction((command)=>{
+            //Ubicaci_on en json, ClaveLuminaria, Cliente,Estatus, Tipo, Latitud, Longitud, FechaTupla, ContratoVigente
+            //d_Padr_on, id_Estado, Voltaje, Clasificaci_on, Tipo, Observaci_on, Evidencia fotos json, Fecha, Usuario
+            command.executeSql(`create table if not exists Luminaria
+                                (id integer primary key NOT NULL,
+                                    Clave varchar,
+                                    Ubicacion varchar, 
+                                    Cliente integer,
+                                    Tipo varchar,
+                                    Latitud varchar,
+                                    Longitud varchar,
+                                    FechaTupla varchar,
+                                    ContratoVigente varchar
+                                    );
+                                    create table if not exists HistorialLuminaria 
+                                    (
+                                        id integer primary key NOT NULL,
+                                        Padron integer,
+                                        Estado integer,
+                                        Voltaje integer,
+                                        Calsificacion varchar,
+                                        Tipo integer,
+                                        Observacion varchar,
+                                        Evidencia varchar,
+                                        Fecha varchar,
+                                        Usuario integer
+                                    );
+                                    create table if not exists HistorialMedidores
+                                    (
+                                        id integer primary key NOT NULL,
+                                        Padron integer,
+                                        Estado integer,
+                                        LecturaActual integer,
+                                        LecturaAnterior integer,
+                                        Consumo integer,
+                                        Observacion varchar,
+                                        Evidencia varchar,
+                                        Fecha varchar,
+                                        Usuario integer
+                                    ) 
+                                    `);
+        },
+        (error)=>{ console.log("Error al crear la base de datos: " + error.message )},
+        ()=>{console.log("Creados correctamente!")});
+    }
+
 
     //NOTE: metodos para guardar datos basicos de 
     public async setUser(user:string, name:string, token: string,cliente:string ){
@@ -16,7 +63,7 @@ export class StorageService{
         console.log("Datos almacenados");
     }
     public async getItem(key:string){
-        return await AsyncStorage.setItem(root+"Cliente",key);
+        return await AsyncStorage.getItem(root+key);
     }
 }
 
