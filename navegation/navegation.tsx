6a654957fcca7bed6624c2f00202c955487b8588
Hector <dango.dark@gmail.com>
2022-02-23 12:56:18 -0600

    librerias inecesarias

--- navegation/navegation.tsx
+++ navegation/navegation.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import { NavigationContainer ,DefaultTheme} from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
 import Home from '../pages/Login';
 import BachesHome from '../pages/Baches/home';
-import { PrimaryColor } from '../Styles/BachesColor';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 const Stack = createStackNavigator();
 function Nav() {
     return(
